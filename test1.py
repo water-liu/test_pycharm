90c314683a577e4fdb487f821f1f66d0c5c5ecc1
liuy-ubuntu <liuyang1990vip@163.com>
2017-08-07 17:32:51 +0800

    Merge branch 'master' of github.com:water-liu/test_pycharm
    # Conflicts:
    #	test1.py

--- test1.py
+++ test1.py
@@ -1,4 +1,25 @@
-print('test git!')
-print('test push 2')
-print('test token')
-print('test push3 ')
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""a test module"""
+
+# __author__ = 'water-liu'
+
+import functools
+import sys
+
+
+def test():
+    args = sys.argv
+    if len(args) == 1:
+        print('Hello world!')
+    elif len(args) == 2:
+        print('Hello, %s!') % args
+    else:
+        print('Too many arguments')
+
+max2 = functools.partial(max, 10)
+print(max2(1, 3, 4, 5))
+if __name__ == '__main__':
+    test()
+
