e1d7f7ad39c31f652a474dfa54a103aca3628f06
liuy-ubuntu <liuyang1990vip@163.com>
2017-08-10 10:09:53 +0800

    Merge branch 'master' of github.com:water-liu/test_pycharm
    # Conflicts:
    #	test1.py

--- test2.py
+++ test2.py
@@ -1,17 +1,44 @@
-#！/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
 
 class Student(object):
 
     def __init__(self, name, score):
-        self.name = name
-        self.score = score
+        self.__name = name
+        self.__score = score
 
     def print_score(self):
-        print("%s : %s" % (self.name, self.score))
+        print("%s : %s" % (self.__name, self.__score))
+
+    def get_name(self):
+        return self.__name
+
+    def get_score(self):
+        return self.__score
+
+    def get_grade(self):
+        if self.__score > 90:
+            print('A')
+        elif self.__score > 60:
+            print('B')
+        else:
+            print('C')
+
+    def set__score(self, score):
+        if 0 <= score <= 100:
+            self.__score = score
+        else:
+            raise ValueError('bad score')
 
 liuy = Student('liuy personal', 44)
-liuq = Student('liuq personla', 88)
+liuq = Student('liuq personal', 88)
+#liuy.set__score(101)
 liuy.print_score()
 liuq.print_score()
+liuy.get_grade()
+print(liuy.get_name())
+print(liuy._Student__name)
+liuy.__name = 'liuy'
+print(liuy.__name)
+print(liuy.get_name())
