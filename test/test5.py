9aeac6979faa06c91bed005aa14ab2a2eaa1005b
liuy-ubuntu <liuyang1990vip@163.com>
2017-08-21 23:03:26 +0800

    添加了property和多重继承

--- test/test5.py
+++ test/test5.py
@@ -28,26 +28,93 @@ from types import MethodType
 # except AttributeError as e:
 #     print('AttributeError:', e)
 
-class Student(object):
-
-    def get_score(self):
-        return self._score
-
-    def set_score(self, value):
-        if not isinstance(value, int):
-            raise ValueError('score need int')
-        if value > 100 or value < 0:
-            raise ValueError('score need 0<score<100')
-        self._score = value
-
-
-s = Student()
-s.set_score(98)
-print(s.get_score())
+# class Student(object):
+#
+#     def get_score(self):
+#         return self._score
+#
+#     def set_score(self, value):
+#         if not isinstance(value, int):
+#             raise ValueError('score need int')
+#         if value > 100 or value < 0:
+#             raise ValueError('score need 0<score<100')
+#         self._score = value
+#
+#
+# s = Student()
+# s.set_score(98)
+# print(s.get_score())
 # try:
 #     s.set_score(888)
 # except AttributeError as e:
 #     print('AttributeError:', e)
+
+# class Student(object):
+#
+#     @property  # 相当于getter
+#     def score(self):
+#         return self._score
+#
+#     @score.setter # 相当于setter
+#     def score(self, value):
+#         if not isinstance(value, int):
+#             raise ValueError('score must be integer')
+#         if value < 0 or value > 100:
+#             raise ValueError('score must be between 0-99')
+#         self._score = value
+#
+# s1 = Student()
+# s1.score = 90
+# print(s1.score)
+
+
+# class People(object):
+#
+#     @property
+#     def birth(self):
+#         return self._birth
+#
+#     @birth.setter
+#     def birth(self, value):
+#         self._birth = value
+#
+#     @property
+#     def age(self):
+#         return 2017 - self._birth
+#
+# p1 = People()
+# p1.birth = 1991
+# print(p1.birth)
+# print(p1.age)
+
+class Screen(object):
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        self._height = value
+
+    @property
+    def resolution(self):
+        return self._width * self._height
+
+
+s = Screen()
+s.width = 1024
+s.height = 768
+print(s.resolution)
+assert s.resolution == 786432, '1024 * 768 = %d ?' % s.resolution
 
 
 
